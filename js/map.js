0880e168943e59dae3c59f6be683ac2fd82091c0
Ellison Leão <ellisonleao@gmail.com>
2017-08-21 01:27:52 -0300

    super changes

--- js/map.js
+++ js/map.js
@@ -1,59 +1,258 @@
-function initMap() {
-    console.log('init map called')
-    var curitiba = {lat: -25.424429, lng: -49.265382};
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 14,
-        draggable: true,
-        center: curitiba
-    });
+$(document).ready(function() {
+    var curitiba = [-25.424429, -49.265382];
+    var icon = L.icon({
+        iconUrl: 'img/marker.png',
+        iconSize: [49,60],
+        iconAnchor: [33, 59],
+        popupAnchor: [-3, -60],
+    })
+    var map = L.map('map', {zoomControl: false}).setView(curitiba, 13);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: '',
+        maxZoom: 13,
+        id: 'mapbox.streets',
+        accessToken: 'pk.eyJ1IjoiZWxsaXNvbmxlYW8iLCJhIjoiY2o2bDkwY2p3MHF0ajJ2cW9yc2hidzEwNiJ9.fkdwlQeT-Wbq2_sYU78l1g'
+    }).addTo(map);
 
-    var districts = {
-        abranches: {lat: -25.370868, lng: -49.270705},
-        ahu: {lat: -25.403990, lng: -49.260186},
-        altodaXV: {lat: -25.427021, lng: -49.250203},
-        bacacheri: {lat: -25.397200, lng: -49.236718},
-        balsaNova: {lat: -25.580861, lng: -49.629558},
-        batel: {lat: -25.444418, lng: -49.288106},
-        bigorrilho: {lat: -25.433261, lng:-49.297157},
-        boaVista: {lat: -25.388682, lng: -49.233469},
-        bomRetiro: {lat: -25.410444, lng: -49.275414},
-        botiatuvinha: {lat: -25.389416, lng: -49.355035},
-        cabral: {lat: -25.406124, lng:-49.252034},
-        campoComprido: {lat:-25.466876, lng: -49.327463},
-        campoLargo: {lat: -25.459671, lng: -49.527294},
-        capaoImbuia: {lat: -25.437300, lng: -49.211194},
-        centro: {lat: -25.424429, lng: -49.265382},
-        centroCivico: {lat: -25.417489, lng: -49.268707},
-        colombo: {lat: -25.292978, lng: -49.226634},
-        cristoRei: {lat: -25.433166 , lng: -49.242973},
-        jardimSocial: {lat: -25.419069, lng: -49.234203},
-        juveve: {lat: -25.412874 , lng: -49.258339},
-        novoMundo: {lat: -25.489025, lng: -49.294125},
-        orleans: {lat: -25.426048, lng: -49.366964},
-        pilarzinho: {lat: -25.392658, lng: -49.282962},
-        santaCandida: {lat: -25.370598, lng: -49.234298},
-        santaFelicidade: {lat: -25.404007, lng: -49.328694},
-        saoBraz: {lat: -25.418977, lng: -49.348347},
-        taruma: {lat: -25.428522 , lng: -49.228371},
-        vilaIsabel: {lat: -25.457769, lng: -49.294653},
-        vistaAlegre: {lat: -25.409061 , lng: -49.291462},
-        portao: {lat: -25.474922 , lng: -49.300784},
-        boqueirao: {lat: -25.503107, lng: -49.236857},
-        aguaVerde: {lat: -25.453071, lng: -49.277923},
-        cajuru: {lat: -25.462060, lng: -49.205588},
-        jardimAmericas: {lat: -25.456324, lng: -49.231621},
-    };
+    var cells = {
+        anaCicilia: {name: 'Anacicilia', day: 'Quarta-Feira', hour: '15:00'},
+        antonioMadeira: {name: 'Pastor Antônio Madeira', day: 'Terça-Feira', hour: '20:00'},
+        clayton: {name: 'Clayton', day: 'Terça-Feira', hour: '20:00'},
+        cleverson: {name: 'Cleverson', day: 'Sexta-Feira', hour: '20:00'},
+        debora: {name: 'Débora', day: 'Terça-Feira', hour: '20:00'},
+        elce: {name: 'Elce', day: 'Terça-Feira', hour: '20:00'},
+        gesualda: {name: 'Gesualda', day: 'Terça-Feira', hour: '20:00'},
+        jesiel: {name: 'Jesiel', day: 'Quinta-Feira', hour: '20:00'},
+        lais: {name: 'Laís', day: 'Sexta-Feira', hour: '20:00'},
+        leonice: {name: 'Leonice', day: 'Terça-Feira', hour: '20:30'},
+        mariaCristina: {name: 'Maria Cristina', day: 'Sexta-Feira', hour: '20:00'},
+        onesimo: {name: 'Onésimo', day: 'Terça-Feira', hour: '20:30'},
+        thelma: {name: 'Thelma', day: 'Terça-Feira', hour: '20:00'},
+        willian: {name: 'Willian', day: 'Terça-Feira', hour: '20:00'},
+    }
 
-    var keys = Object.keys(districts);
+    var districts = [
+        {
+            pos: [-25.370868, -49.270705],
+            name: 'Abranches',
+            cells: [cells.cleverson],
+        },
+        {
+            pos: [-25.453071, -49.277923],
+            name: 'Água Verde',
+            cells: [cells.antonioMadeira],
+        },
+        {
+            pos: [-25.403990, -49.260186],
+            name: 'Ahú',
+            cells: [cells.leonice],
+        },
+        {
+            pos: [-25.427021, -49.250203],
+            name: 'Alto da XV',
+            cells: [cells.thelma]
+        },
+        {
+            pos: [-25.397200, -49.236718],
+            name: 'Bacacheri',
+            cells: [cells.onesimo, cells.leonice]
+        },
+        {
+            pos: [-25.580861, -49.629558],
+            name: 'Balsa Nova',
+            cells: [cells.cleverson]
+        },
+        {
+            pos: [-25.444418, -49.288106],
+            name: 'Batel',
+            cells: [cells.thelma],
+        },
+        {
+            pos: [-25.433261, -49.297157],
+            name: 'Bigorrilho',
+            cells: [cells.onesimo, cells.jesiel],
+        },
+        {
+            pos: [-25.388682, -49.233469],
+            name: 'Boa Vista',
+            cells: [cells.cleverson, cells.leonice],
+        },
+        {
+            pos: [-25.410444, -49.275414],
+            name: 'Bom Retiro',
+            cells: [cells.cleverson],
+        },
+        {
+            pos: [-25.503107, -49.236857],
+            name: 'Boqueirão',
+            cells: [cells.antonioMadeira],
+        },
+        {
+            pos: [-25.389416, -49.355035],
+            name: 'Botiatuvinha',
+            cells: [cells.mariaCristina],
+        },
+        {
+            pos: [-25.406124, -49.252034],
+            name: 'Cabral',
+            cells: [cells.jesiel, cells.leonice],
+        },
+        {
+            pos: [-25.462060, -49.205588],
+            name: 'Cajuru',
+            cells: [cells.debora],
+        },
+        {
+            pos: [-25.466876, -49.327463],
+            name: 'Campo Comprido',
+            cells: [cells.lais, cells.mariaCristina, cells.clayton],
+        },
+        {
+            pos: [-25.459671, -49.527294],
+            name: 'Campo Largo',
+            cells: [cells.cleverson],
+        },
+        {
+            pos: [-25.437300, -49.211194],
+            name: 'Capão da Imbuia',
+            cells: [cells.cleverson, cells.leonice],
+        },
+        {
+            pos: [-25.424429, -49.265382],
+            name: 'Centro',
+            cells: [cells.onesimo]
+        },
+        {
+            pos: [-25.417489, -49.268707],
+            name: 'Centro Cívico',
+            cells: [cells.onesimo, cells.gesualda],
+        },
+        {
+            pos: [-25.292978, -49.226634],
+            name: 'Colombo',
+            cells: [cells.cleverson],
+        },
+        {
+            pos: [-25.433166 , -49.242973],
+            name: 'Cristo Rei',
+            cells: [cells.antonioMadeira]
+        },
+        {
+            pos: [-25.456324, -49.231621],
+            name: 'Jardim das Américas',
+            cells: [cells.willian]
+        },
+        {
+            pos: [-25.419069, -49.234203],
+            name: 'Jardim Social',
+            cells: [cells.onesimo],
+        },
+        {
+            pos: [-25.412874 , -49.258339],
+            name: 'Juvevê',
+            cells: [cells.antonioMadeira]
+        },
+        {
+            pos: [-25.489025, -49.294125],
+            name: 'Novo Mundo',
+            cells: [cells.antonioMadeira],
+        },
+        {
+            pos: [-25.426048, -49.366964],
+            name: 'Orleans',
+            cells: [cells.mariaCristina]
+        },
+        {
+            pos: [-25.392658, -49.282962],
+            name: 'Pilarzinho',
+            cells: [cells.lais],
+        },
+        {
+            pos: [-25.474922, -49.300784],
+            name: 'Portão',
+            cells: [cells.antonioMadeira]
+        },
+        {
+            pos: [-25.370598, -49.234298],
+            name: 'Santa Cândida',
+            cells: [cells.lais]
+        },
+        {
+            pos: [-25.404007, -49.328694],
+            name: 'Santa Felicidade',
+            cells: [cells.debora, cells.mariaCristina],
+        },
+        {
+            pos: [-25.418977, -49.348347],
+            name: 'São Braz',
+            cells: [cells.mariaCristina],
+        },
+        {
+            pos: [-25.428522, -49.228371],
+            name: 'Tarumã',
+            cells: [cells.leonice]
+        },
+        {
+            pos: [-25.457769, -49.294653],
+            name: 'Vila Isabel',
+            cells: [cells.antonioMadeira],
+        },
+        {
+            pos: [-25.409061, -49.291462],
+            name: 'Vista Alegre',
+            cells: [cells.onesimo]
+        },
+    ];
+
+    var html = '';
     var markers = [];
-    keys.forEach(function(item) {
-        var marker = new google.maps.Marker({
-            position: districts[item],
-            icon: '/img/marker.png',
-        });
+    districts.forEach(function(d, i) {
+        var cellText = '<h4>'+d.name+'</h4><table class="mdl-data-table mdl-js-data-table"><thead><tr class="mdl-data-table__cell--non-numeric"><th>Nome do Responsável</th><th>Dia</th><th>Hora</th></tr></thead><tbody>';
+        d.cells.forEach(function(cell) {
+            cellText += '<tr><td class="mdl-data-table__cell--non-numeric">'+cell.name+'</td><td class="mdl-data-table__cell--non-numeric">'+cell.day+'</td><td class="mdl-data-table__cell--non-numeric">'+cell.hour+'</td></tr>';
+        })
+        cellText += '</tbody></table>';
+
+        var marker = L.marker(d.pos, {
+            id: 'marker-'+ i,
+            icon: icon,
+            riseOnHover: true,
+        }).addTo(map).bindPopup(cellText).on('click', clickZoom);
+        // populate districts menu
+        html += '<a id="marker-'+i+'"class="mdl-navigation__link">'+ d.name +'</a>'
         markers.push(marker);
     });
-    // Add a marker clusterer to manage the markers.
-    var markerCluster = new MarkerClusterer(map, markers, {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
-}
 
+    function clickZoom(e) {
+        map.setView(e.target.getLatLng(), 14);
+    }
+
+    function markerFunction(id) {
+        markers.forEach(function(marker) {
+            var markerId = marker.options.id;
+            if (markerId === id){
+                $(".mdl-layout__drawer-button").click();
+                map.setView(marker.getLatLng(), 14);
+                marker.openPopup();
+            };
+        });
+    }
+    $(document).on('click', 'a', function(e) {
+        markerFunction($(e.target)[0].id)
+    })
+
+    $('#bairros').html(html);
+
+    var dialog = $('dialog')[0];
+    var showModalButton = $('.show-dialog');
+    if (!dialog.showModal) {
+      dialogPolyfill.registerDialog(dialog);
+    }
+    showModalButton.on('click', function() {
+        console.log('CLICKED')
+      dialog.showModal();
+    });
+    $('.close').on('click', function() {
+      dialog.close();
+    });
+});
